a01ce9a4b85ec6b43d0112ea240f50915c2de1fe
TyRoXx <tyroxxxx@gmail.com>
2014-11-30 17:12:52 +0100

    extract a loop as a function

--- examples/lode_webserver.lua
+++ examples/lode_webserver.lua
@@ -1,17 +1,23 @@
+local sync_for_each = function (observable, handler)
+	while true do
+		local element = observable:sync_get()
+		if element == nil then
+			break
+		end
+		handler(element)
+	end
+end
+
 return function (require)
 	local web = require("web", "2.0")
 	local visitor_count = 0
 	local requests = web.create_server(8080)
-	while true do
-		local response = requests:sync_get()
-		if response == nil then
-			break
-		end
+	sync_for_each(requests, function (response)
 		coroutine.resume(coroutine.create(function ()
 			response:add_header("Content-Type", "text/html")
 			response:add_header("Connection", "close")
 			visitor_count = visitor_count + 1
 			response:set_content("Hello, world!<br>Visitor number: " .. tostring(visitor_count))
 		end))
-	end
+	end)
 end
