452042d6e54ed9118fd367cc5fbb3dbfe0fc6959
TyRoXx <tyroxxxx@gmail.com>
2014-12-09 17:04:40 +0100

    first test for add_method

--- test/lua_wrapper.cpp
+++ test/lua_wrapper.cpp
@@ -476,3 +476,40 @@ BOOST_AUTO_TEST_CASE(lua_wrapper_coroutine_finish)
 		BOOST_CHECK_EQUAL(static_cast<lua_Integer>(23), coro_stack.get_integer(*return_values));
 	});
 }
+
+BOOST_AUTO_TEST_CASE(lua_wrapper_add_method_trivial)
+{
+	test_with_environment([](lua::stack &s, resource bound)
+	{
+		struct test_struct
+		{
+			bool *called;
+
+			explicit test_struct(bool &called)
+				: called(&called)
+			{
+			}
+
+			void method()
+			{
+				BOOST_CHECK(called);
+				BOOST_REQUIRE(!*called);
+				*called = true;
+			}
+		};
+
+		lua::stack_value meta = lua::create_default_meta_table<test_struct>(s);
+		lua::add_method(s, meta, "method", &test_struct::method);
+
+		bool called = false;
+		lua::stack_value object = lua::emplace_object<test_struct>(s, meta, called);
+		
+		lua_getfield(s.state(), -1, "method");
+
+		lua::push(*s.state(), object);
+
+		BOOST_REQUIRE(!called);
+		lua::pcall(*s.state(), 1, boost::none);
+		BOOST_CHECK(called);
+	});
+}
