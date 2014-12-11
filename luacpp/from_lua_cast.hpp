b5187715cd4e33ccbd63ebd586bb20c4a769f1f9
TyRoXx <tyroxxxx@gmail.com>
2014-12-11 22:19:22 +0100

    specialize from_lua for fast_variant (not very efficiently yet)

--- luacpp/from_lua_cast.hpp
+++ luacpp/from_lua_cast.hpp
@@ -2,6 +2,7 @@
 #define LUACPP_FROM_LUA_CAST_HPP
 
 #include "luacpp/stack_value.hpp"
+#include "luacpp/stack.hpp"
 #include "luacpp/reference.hpp"
 #include <silicium/memory_range.hpp>
 
@@ -13,6 +14,8 @@ namespace lua
 	template <>
 	struct from_lua<lua_Number>
 	{
+		static type const lua_type = type::number;
+
 		lua_Number operator()(lua_State &L, int address) const
 		{
 			return lua_tonumber(&L, address);
@@ -22,6 +25,8 @@ namespace lua
 	template <>
 	struct from_lua<lua_Integer>
 	{
+		static type const lua_type = type::number;
+
 		lua_Integer operator()(lua_State &L, int address) const
 		{
 			return lua_tointeger(&L, address);
@@ -31,6 +36,8 @@ namespace lua
 	template <>
 	struct from_lua<bool>
 	{
+		static type const lua_type = type::boolean;
+
 		bool operator()(lua_State &L, int address) const
 		{
 			return lua_toboolean(&L, address) != 0;
@@ -40,6 +47,8 @@ namespace lua
 	template <>
 	struct from_lua<void *>
 	{
+		static type const lua_type = type::user_data;
+
 		void *operator()(lua_State &L, int address) const
 		{
 			return lua_touserdata(&L, address);
@@ -49,6 +58,8 @@ namespace lua
 	template <>
 	struct from_lua<Si::noexcept_string>
 	{
+		static type const lua_type = type::string;
+
 		Si::noexcept_string operator()(lua_State &L, int address) const
 		{
 			char const *raw = lua_tostring(&L, address);
@@ -63,9 +74,45 @@ namespace lua
 	template <>
 	struct from_lua<char const *>
 	{
+		static type const lua_type = type::string;
+
 		char const *operator()(lua_State &L, int address) const
 		{
 			return lua_tostring(&L, address);
+		}
+	};
+
+	template <class ...T>
+	struct from_lua<Si::fast_variant<T...>>
+	{
+		typedef Si::fast_variant<T...> variant;
+
+		variant operator()(lua_State &L, int address) const
+		{
+			static std::array<std::pair<variant, bool> (*)(any_local), sizeof...(T)> const converters =
+			{{
+				&try_convert<T>...
+			}};
+			std::pair<variant, bool> result = converters[0](any_local(L, address));
+			for (auto const &converter : converters)
+			{
+				if (result.second)
+				{
+					break;
+				}
+				result = converter(any_local(L, address));
+			}
+			return std::move(result.first);
+		}
+
+	private:
+
+		template <class To>
+		static std::pair<variant, bool> try_convert(any_local from)
+		{
+			auto const type = stack(*from.thread()).get_type(from);
+			bool is_correct_type = (from_lua<To>::lua_type == type);
+			return std::make_pair(variant(from_lua<To>()(*from.thread(), from.from_bottom())), is_correct_type);
 		}
 	};
 
